440638334a80acaa0b352091b37e02287e45ae59
Mailesh-B <bmailesh2020@gmail.com>
2024-10-12 14:19:10 +0530

    first commit

---
+++ src/Oct/Tasks/Task_03102024/simplecalculator.java
@@ -0,0 +1,11 @@
+package Oct.Tasks.Task_03102024;
+
+public class simplecalculator {
+    public static void main(String[] args) {
+        System.out.println("Simple Calculator Program");
+        System.out.println(20+456);
+        System.out.println(70-396);
+        System.out.println(30*542);
+        System.out.println(10/356);
+    }
+}
---
+++ src/Oct/Tasks/Task_03102024/tableof10.java
@@ -0,0 +1,18 @@
+package Oct.Tasks.Task_03102024;
+
+public class tableof10 {
+    public static void main(String[] args) {
+        System.out.println("Table of 10");
+        System.out.println("10X1=10");
+        System.out.println("10X2=20");
+        System.out.println("10X3=30");
+        System.out.println("10X4=40");
+        System.out.println("10X5=50");
+        System.out.println("10X6=60");
+        System.out.println("10X7=70");
+        System.out.println("10X8=80");
+        System.out.println("10X9=90");
+        System.out.println("10X10=100");
+    }
+
+}
